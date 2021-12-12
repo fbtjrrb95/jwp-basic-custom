cbb0fff2a6008d9e1a1eee943d9ceb7a39c92d12
seokkyu <tjrrb95@gmail.com>
2021-12-12 23:14:06 +0900

    feat(update_user): update user servlet 파일 생성

---
+++ src/main/java/next/web/UpdateUserServlet.java
@@ -0,0 +1,17 @@
+package next.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/user/update")
+public class UpdateUserServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    } 
+}
