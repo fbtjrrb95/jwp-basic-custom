9ddf545b0c2ab1520268a6e51a0fc9888d8e0bec
seokkyu <tjrrb95@gmail.com>
2021-12-14 23:58:18 +0900

    feat(update_user): url 변경

--- src/main/java/next/web/UpdateUserFormServlet.java
+++ src/main/java/next/web/UpdateUserFormServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/user/update")
+@WebServlet("/user/form")
 public class UpdateUserFormServlet extends HttpServlet {
 
     @Override
