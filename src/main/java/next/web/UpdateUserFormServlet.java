5ded8b83c3d748c1f4c50e4ac7c8aba7c5248b56
seokkyu <tjrrb95@gmail.com>
2021-12-15 23:51:32 +0900

    feat(update_user): logging

--- src/main/java/next/web/UpdateUserFormServlet.java
+++ src/main/java/next/web/UpdateUserFormServlet.java
@@ -1,5 +1,8 @@
 package next.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,10 +14,17 @@ import java.io.IOException;
 @WebServlet("/user/form")
 public class UpdateUserFormServlet extends HttpServlet {
 
+    Logger log = LoggerFactory.getLogger(UpdateUserFormServlet.class);
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        log.info("request : {}", request);
+
         RequestDispatcher requestDispatcher = request.getRequestDispatcher("/user/update.jsp");
         requestDispatcher.forward(request, response);
+
+        log.info("response : {}", response);
     }
 
 }
