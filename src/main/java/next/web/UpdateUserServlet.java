c2a67df028499358236667bfd7de7e0fbca51921
seokkyu <tjrrb95@gmail.com>
2021-12-17 23:37:56 +0900

    feat(update_user): user update servlet prototype 로직 구현.

--- src/main/java/next/web/UpdateUserServlet.java
+++ src/main/java/next/web/UpdateUserServlet.java
@@ -1,5 +1,9 @@
 package next.web;
 
+import core.db.DataBase;
+import next.model.User;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,6 +16,23 @@ public class UpdateUserServlet extends HttpServlet {
 
     @Override
     protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String userId = request.getParameter("userId");
+        String name = request.getParameter("name");
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
 
+        RequestDispatcher requestDispatcher;
+        if (userId.isEmpty() || name.isEmpty() || email.isEmpty() || password.isEmpty()) {
+            requestDispatcher = request.getRequestDispatcher("/user/update.jsp");
+            requestDispatcher.forward(request, response);
+            return;
+        }
+
+        User user = DataBase.findUserById(userId);
+        user.setEmail(email);
+        user.setName(name);
+        user.setPassword(password);
+
+        DataBase.addUser(user);
     }
 }
