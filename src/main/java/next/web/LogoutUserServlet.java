95a9803167645164c29da7ad98f422ddcb38c192
seokkyu <tjrrb95@gmail.com>
2021-12-30 23:58:46 +0900

    feat(login-logout): add log out servlet skeleton code

--- src/main/java/next/web/LogoutUserServlet.java
+++ src/main/java/next/web/LogoutUserServlet.java
@@ -1,4 +1,18 @@
 package next.web;
 
-public class LogoutUserServlet {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class LogoutUserServlet extends HttpServlet {
+    private static final Logger log = LoggerFactory.getLogger(LogoutUserServlet.class);
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    }
 }
