47e6bbaac0812e18a50d22c3de6479a55cea4bd7
seokkyu <tjrrb95@gmail.com>
2022-01-01 23:56:47 +0900

    feat(login-logout): logout 시 session 제거 하는 로직 추가.

--- src/main/java/next/web/LogoutUserServlet.java
+++ src/main/java/next/web/LogoutUserServlet.java
@@ -1,12 +1,16 @@
 package next.web;
 
+import core.db.DataBase;
+import next.model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 public class LogoutUserServlet extends HttpServlet {
@@ -14,5 +18,11 @@ public class LogoutUserServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        HttpSession session = request.getSession();
+        session.removeAttribute("user");
+
+        response.sendRedirect("/index.jsp");
+
     }
 }
