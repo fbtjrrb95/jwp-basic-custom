813f879b79dbef2a51098e87faa7964159642cd8
seokkyu <tjrrb95@gmail.com>
2022-02-09 23:32:43 +0900

    feat(cool-down): logout controller 프로토타입 생성.

---
+++ src/main/java/next/controller/LogoutController.java
@@ -0,0 +1,8 @@
+package next.controller;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("/logout")
+public class LogoutController extends HttpServlet {
+}
