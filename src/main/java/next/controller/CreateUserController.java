f85c2200e54a24551eb7c04932cbf0d7ef95a215
seokkyu <tjrrb95@gmail.com>
2022-04-11 22:28:57 +0900

    feat(): jdbc 이용하여 user 생성

--- src/main/java/next/controller/CreateUserController.java
+++ src/main/java/next/controller/CreateUserController.java
@@ -1,6 +1,6 @@
 package next.controller;
 
-import core.db.DataBase;
+import next.dao.UserDao;
 import next.model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.sql.SQLException;
 
 public class CreateUserController implements Controller {
     private static final Logger log = LoggerFactory.getLogger(CreateUserController.class);
@@ -19,7 +20,13 @@ public class CreateUserController implements Controller {
                 request.getParameter("name"),
                 request.getParameter("email"));
         log.debug("user : {}", user);
-        DataBase.addUser(user);
+//        DataBase.addUser(user);
+        UserDao userDao = new UserDao();
+        try {
+            userDao.insert(user);
+        } catch (SQLException e) {
+            log.error(e.getMessage());
+        }
 
         HttpSession session = request.getSession();
         session.setAttribute("user", user);
