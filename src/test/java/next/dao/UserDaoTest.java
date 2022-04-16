fa65ac281693c7fb9bf546d47dd87bf3aacfb240
seokkyu <tjrrb95@gmail.com>
2022-04-17 00:13:40 +0900

    feat(): user dao update test code 수정

--- src/test/java/next/dao/UserDaoTest.java
+++ src/test/java/next/dao/UserDaoTest.java
@@ -2,16 +2,24 @@ package next.dao;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import next.model.User;
 
 public class UserDaoTest {
 
+    private UserDao userDao = new UserDao();
+
+    @Before
+    public void init() throws Exception {
+        UserDao.truncate();
+    }
+
     @Test
     public void create() throws Exception {
         User expected = new User("userId1", "password", "name", "javajigi@email.com");
-        UserDao userDao = new UserDao();
         userDao.insert(expected);
 
         User actual = userDao.findByUserId(expected.getUserId());
@@ -21,13 +29,13 @@ public class UserDaoTest {
 
     @Test
     public void update() throws Exception {
-        User expected = new User("userId", "password", "name", "javajigi@email.com");
-        UserDao userDao = new UserDao();
+        User expected = new User("userId1", "password", "name", "javajigi@email.com");
         userDao.insert(expected);
 
         String newName = "newName";
         expected.setName(newName);
-        userDao.update(expected);
+
+        userDao.update(expected.getUserId(), expected);
 
         User updated = userDao.findByUserId(expected.getUserId());
         assertEquals(updated.getName(), newName);
