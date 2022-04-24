5b868904c006311be60072035c7562f8469b543f
seokkyu <tjrrb95@gmail.com>
2022-04-24 23:29:56 +0900

    fix typo

--- src/main/java/next/controller/CreateUserController.java
+++ src/main/java/next/controller/CreateUserController.java
@@ -26,7 +26,7 @@ public class CreateUserController implements Controller {
             userDao.insert(user);
         } catch (SQLException e) {
             log.error(e.getMessage());
-            return "reierect:/users/create";
+            return "redirect:/users/create";
         }
 
         HttpSession session = request.getSession();
