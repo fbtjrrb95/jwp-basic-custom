91abf5ec2e6fafed9f4a3f73c3b319680a776d5d
seokkyu <tjrrb95@gmail.com>
2022-05-10 23:46:46 +0900

    사용하지 않는 코드 삭제

--- src/main/java/next/dao/UserDao.java
+++ src/main/java/next/dao/UserDao.java
@@ -29,49 +29,6 @@ public class UserDao {
         insertJdbcTemplate.insert(user);
     }
 
-    void setValuesForInsert(User user, PreparedStatement preparedStatement) throws SQLException {
-        preparedStatement.setString(1, user.getUserId());
-        preparedStatement.setString(2, user.getPassword());
-        preparedStatement.setString(3, user.getName());
-        preparedStatement.setString(4, user.getEmail());
-    }
-
-    String createQueryForInsert() {
-        return "INSERT INTO USERS VALUES (?, ?, ?, ?)";
-    }
-
-    public User findByUserId(String userId) throws SQLException {
-        Connection con = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-        try {
-            con = ConnectionManager.getConnection();
-            String sql = "SELECT userId, password, name, email FROM USERS WHERE userid=?";
-            pstmt = con.prepareStatement(sql);
-            pstmt.setString(1, userId);
-
-            rs = pstmt.executeQuery();
-
-            User user = null;
-            if (rs.next()) {
-                user = new User(rs.getString("userId"), rs.getString("password"), rs.getString("name"),
-                        rs.getString("email"));
-            }
-
-            return user;
-        } finally {
-            if (rs != null) {
-                rs.close();
-            }
-            if (pstmt != null) {
-                pstmt.close();
-            }
-            if (con != null) {
-                con.close();
-            }
-        }
-    }
-
     public static List<User> findAll() throws SQLException {
         Connection con = null;
         PreparedStatement pstmt = null;
