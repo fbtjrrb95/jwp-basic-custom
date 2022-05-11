9e35ea262a446aca25ddb7daff52ef0fc65eb21e
seokkyu <tjrrb95@gmail.com>
2022-05-11 23:59:15 +0900

    find by user id function roll back

--- src/main/java/next/dao/UserDao.java
+++ src/main/java/next/dao/UserDao.java
@@ -29,6 +29,38 @@ public class UserDao {
         insertJdbcTemplate.insert(user);
     }
 
+    public User findByUserId(String userId) throws SQLException {
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        try {
+            con = ConnectionManager.getConnection();
+            String sql = "SELECT userId, password, name, email FROM USERS WHERE userid=?";
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, userId);
+
+            rs = pstmt.executeQuery();
+
+            User user = null;
+            if (rs.next()) {
+                user = new User(rs.getString("userId"), rs.getString("password"), rs.getString("name"),
+                        rs.getString("email"));
+            }
+
+            return user;
+        } finally {
+            if (rs != null) {
+                rs.close();
+            }
+            if (pstmt != null) {
+                pstmt.close();
+            }
+            if (con != null) {
+                con.close();
+            }
+        }
+    }
+
     public static List<User> findAll() throws SQLException {
         Connection con = null;
         PreparedStatement pstmt = null;
