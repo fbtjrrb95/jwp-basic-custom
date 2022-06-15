59ad9760b01515477a10c064867be59a4076cb42
seokkyu <tjrrb95@gmail.com>
2022-06-16 01:26:03 +0900

    chore(): delete unused code

--- src/main/java/next/dao/SelectJdbcTemplate.java
+++ src/main/java/next/dao/SelectJdbcTemplate.java
@@ -11,37 +11,6 @@ import java.util.List;
 
 public abstract class SelectJdbcTemplate {
 
-    @SuppressWarnings("rawtypes")
-    public List query(String sql) throws SQLException {
-        Connection con = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-        try {
-            con = ConnectionManager.getConnection();
-            pstmt = con.prepareStatement(sql);
-            setValues(pstmt);
-
-            rs = pstmt.executeQuery();
-
-            List<Object> result = new ArrayList<>();
-            while (rs.next()) {
-                result.add(mapRow(rs));
-            }
-            return result;
-        } finally {
-            if (rs != null) {
-                rs.close();
-            }
-            if (pstmt != null) {
-                pstmt.close();
-            }
-            if (con != null) {
-                con.close();
-            }
-        }
-
-    }
-
     abstract Object mapRow(ResultSet rs) throws SQLException;
     abstract void setValues(PreparedStatement pstmt);
 }
