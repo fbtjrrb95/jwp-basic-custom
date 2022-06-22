b85554233462fd398b7a845c1b869930725c54f3
seokkyu <tjrrb95@gmail.com>
2022-06-22 23:46:06 +0900

    refactor(): use try catch with resources

--- src/main/java/next/dao/JdbcTemplate.java
+++ src/main/java/next/dao/JdbcTemplate.java
@@ -34,12 +34,12 @@ public abstract class JdbcTemplate {
 
     @SuppressWarnings("rawtypes")
     public List query(String sql) throws SQLException {
-        Connection con = null;
-        PreparedStatement pstmt = null;
+
         ResultSet rs = null;
-        try {
-            con = ConnectionManager.getConnection();
-            pstmt = con.prepareStatement(sql);
+        try (
+                Connection con = ConnectionManager.getConnection();
+                PreparedStatement pstmt = con.prepareStatement(sql);
+        ){
             setValues(pstmt);
 
             rs = pstmt.executeQuery();
@@ -49,18 +49,7 @@ public abstract class JdbcTemplate {
                 result.add(mapRow(rs));
             }
             return result;
-        } finally {
-            if (rs != null) {
-                rs.close();
-            }
-            if (pstmt != null) {
-                pstmt.close();
-            }
-            if (con != null) {
-                con.close();
-            }
         }
-
     }
 
     @SuppressWarnings("rawTypes")
