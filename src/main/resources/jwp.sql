6971dfb4e02ede1438fc329b5a567e508a5edf56
seokkyu <tjrrb95@gmail.com>
2022-07-17 01:10:13 +0900

    feat(): add q&a table ddl

--- src/main/resources/jwp.sql
+++ src/main/resources/jwp.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS USERS;
 
 CREATE TABLE USERS ( 
-	userId          varchar(12)		NOT NULL, 
+    userId          varchar(12)		NOT NULL,
 	password		varchar(12)		NOT NULL,
 	name			varchar(20)		NOT NULL,
 	email			varchar(50),	
@@ -10,3 +10,26 @@ CREATE TABLE USERS (
 );
 
 INSERT INTO USERS VALUES('admin', 'password', '자바지기', 'admin@slipp.net');
+
+DROP TABLE IF EXISTS QUESTIONS;
+
+CREATE TABLE QUESTIONS (
+    questionId 			bigint				auto_increment,
+    writer				varchar(30)			NOT NULL,
+    title				varchar(50)			NOT NULL,
+    contents			varchar(5000)		NOT NULL,
+    createdDate			timestamp			NOT NULL,
+    countOfAnswer int,
+    PRIMARY KEY               (questionId)
+);
+
+DROP TABLE IF EXISTS ANSWERS;
+
+CREATE TABLE ANSWERS (
+     answerId 			bigint				auto_increment,
+     writer				varchar(30)			NOT NULL,
+     contents			varchar(5000)		NOT NULL,
+     createdDate			timestamp			NOT NULL,
+     questionId			bigint				NOT NULL,
+     PRIMARY KEY         (answerId)
+);
