0c3594b83920e1fc3d2a1425f4a07835234e4a41
seokkyu <tjrrb95@gmail.com>
2022-07-21 00:24:06 +0900

    feat(): add script add answer func

--- webapp/js/scripts.js
+++ webapp/js/scripts.js
@@ -1,6 +1,6 @@
 
-$(document).ready(function(){/* jQuery toggle layout */
-$('#btnToggle').click(function(){
+$(document).ready(function() {/* jQuery toggle layout */
+$('#btnToggle').click(function() {
   if ($(this).hasClass('on')) {
     $('#main .col-md-6').addClass('col-md-4').removeClass('col-md-6');
     $(this).removeClass('on');
@@ -10,4 +10,26 @@ $('#btnToggle').click(function(){
     $(this).addClass('on');
   }
 });
+
+$(".answerWrite input[type=submit]").click(addAnswer);
+
+function addAnswer(e) {
+  e.preventDefault();
+  const queryString = $("form[name=answer]").serialize();
+
+  $.ajax({
+    type: 'post',
+    url: 'qna/answer',
+    data: queryString,
+    dataType: 'json',
+    error: onError,
+    success: onSuccess,
+  })
+}
+
+function onSuccess(json, status) {
+  const answerTemplate = $("#answerTemplate").html();
+  const template = answerTemplate.format(json.writer, new Date(json.createdDate), json.contents, json.answerId);
+  $(".qna-comment-slipp-articles").prepend(template);
+}
 });
