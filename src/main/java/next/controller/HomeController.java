c035d56906270bded427e5e68f485573b4049b9a
seokkyu <tjrrb95@gmail.com>
2022-07-24 00:13:00 +0900

    feat(): add check http method logic at home controller

--- src/main/java/next/controller/HomeController.java
+++ src/main/java/next/controller/HomeController.java
@@ -1,11 +1,17 @@
 package next.controller;
 
+import javassist.NotFoundException;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Locale;
 
 public class HomeController implements Controller {
     @Override
     public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        return "/home.jsp";
+        if (Method.valueOf(request.getMethod().toUpperCase(Locale.ROOT)).equals(Method.GET)) {
+            return "/home.jsp";
+        }
+        throw new NotFoundException("NOT FOUND");
     }
 }
