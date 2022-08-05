fc887a0ea17c1c28f128843e5c2ed568bbd4cae5
seokkyu <tjrrb95@gmail.com>
2022-08-05 23:54:50 +0900

    feat(): add check method function

--- src/main/java/next/controller/Controller.java
+++ src/main/java/next/controller/Controller.java
@@ -2,8 +2,17 @@ package next.controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Locale;
 
 
 public interface Controller {
     String execute(HttpServletRequest request, HttpServletResponse response) throws Exception;
+
+    default boolean isGet(HttpServletRequest request) {
+        return Method.valueOf(request.getMethod().toUpperCase(Locale.ROOT)).equals(Method.GET);
+    }
+
+    default boolean isPost(HttpServletRequest request) {
+        return Method.valueOf(request.getMethod().toUpperCase(Locale.ROOT)).equals(Method.POST);
+    }
 }
