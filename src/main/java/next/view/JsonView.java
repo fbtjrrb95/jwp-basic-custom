c878f5153f93023a7cd2de0ee2e2f263bdefd689
seokkyu <tjrrb95@gmail.com>
2022-10-31 23:46:47 +0900

    feat(): create json view file

---
+++ src/main/java/next/view/JsonView.java
@@ -0,0 +1,11 @@
+package next.view;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class JsonView implements View {
+    @Override
+    public void render(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+    }
+}
