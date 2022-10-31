27eb8ff1c99df340fc70be3f2dcac18995973b4d
seokkyu <tjrrb95@gmail.com>
2022-11-01 00:15:45 +0900

    feat(): change return json view at answer controller

--- src/main/java/next/controller/AnswerController.java
+++ src/main/java/next/controller/AnswerController.java
@@ -1,23 +1,20 @@
 package next.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import javassist.NotFoundException;
 import next.dao.AnswerDao;
 import next.model.Answer;
+import next.view.JsonView;
 import next.view.View;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.PrintWriter;
 import java.sql.Timestamp;
 import java.time.Instant;
 
 public class AnswerController implements Controller {
-    private static final Logger log =
-            LoggerFactory.getLogger(AnswerController.class);
-    private static final String CONTENT_TYPE = "application/json;charset=UTF-8";
+    private static final Logger log = LoggerFactory.getLogger(AnswerController.class);
     @Override
     public View execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
         if (isPost(request)) {
@@ -31,11 +28,8 @@ public class AnswerController implements Controller {
 
             AnswerDao answerDao = new AnswerDao();
             Answer savedAnswer = answerDao.save(answer);
-            ObjectMapper objectMapper = new ObjectMapper();
-            response.setContentType(CONTENT_TYPE);
-            PrintWriter printWriter = response.getWriter();
-            printWriter.print(objectMapper.writeValueAsString(savedAnswer));
-            return null;
+            request.setAttribute("answer", savedAnswer);
+            return new JsonView();
         }
         throw new NotFoundException("NOT FOUND");
     }
