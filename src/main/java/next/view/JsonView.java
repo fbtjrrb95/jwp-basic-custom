eb11c8907fb3a3267b450f5c221a91f6861299ce
seokkyu <tjrrb95@gmail.com>
2023-03-27 01:21:37 +0900

    feat(JsonView): use singleton object mapper

--- src/main/java/next/view/JsonView.java
+++ src/main/java/next/view/JsonView.java
@@ -1,6 +1,7 @@
 package next.view;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import core.util.ObjectMapperFactory;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,7 +12,7 @@ import java.util.Map;
 public class JsonView implements View {
     @Override
     public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
-        ObjectMapper objectMapper = new ObjectMapper();
+        ObjectMapper objectMapper = ObjectMapperFactory.getInstance();
         response.setContentType("application/json;charset=UTF-8");
         PrintWriter printWriter = response.getWriter();
         printWriter.print(objectMapper.writeValueAsString(model));
