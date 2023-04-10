dc38667802a24e2a71caded99433588888348f36
seokkyu <tjrrb95@gmail.com>
2023-04-10 22:41:29 +0900

    feat(): modify list user return value without login

--- src/main/java/next/controller/ListUserController.java
+++ src/main/java/next/controller/ListUserController.java
@@ -3,8 +3,6 @@ package next.controller;
 import next.dao.UserDao;
 import next.util.UserSessionUtils;
 import next.view.ModelAndView;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -13,12 +11,11 @@ import java.io.IOException;
 import java.sql.SQLException;
 
 public class ListUserController extends AbstractController {
-    private static final Logger log = LoggerFactory.getLogger(ListUserController.class);
 
     @Override
     public ModelAndView execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, SQLException {
         if (!UserSessionUtils.isLogined(request.getSession())) {
-            return jspView("redirect:/login");
+            return jspView("/user/login.jsp");
         }
 
         UserDao userDao = new UserDao();
