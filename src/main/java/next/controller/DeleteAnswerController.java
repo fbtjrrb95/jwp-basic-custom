a24fab241f6a742e83bd617b2ce38fcba2a00138
seokkyu <tjrrb95@gmail.com>
2023-05-05 22:40:11 +0900

    feat(): add logic to decrease answer count at delete answer controller

--- src/main/java/next/controller/DeleteAnswerController.java
+++ src/main/java/next/controller/DeleteAnswerController.java
@@ -1,7 +1,7 @@
 package next.controller;
 
 import core.exception.NotFoundException;
-import next.dao.AnswerDao;
+import next.service.AnswerService;
 import next.view.ModelAndView;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,6 +11,8 @@ import javax.servlet.http.HttpServletResponse;
 
 public class DeleteAnswerController extends AbstractController {
     private static final Logger log = LoggerFactory.getLogger(DeleteAnswerController.class);
+
+    private final AnswerService answerService = new AnswerService();
 
     @Override
     public ModelAndView execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
@@ -23,9 +25,8 @@ public class DeleteAnswerController extends AbstractController {
                 throw new NotFoundException("NOT FOUND");
             }
             log.debug("delete answer id: {}", answerId);
-            AnswerDao answerDao = new AnswerDao();
-            answerDao.delete(answerId);
-            // TODO: decrease answerCount of target question
+            answerService.delete(answerId);
+
             return jsonView();
     }
 }
