f0ad71410f66101f95a7dbeed72c4760ac3e82bf
seokkyu <tjrrb95@gmail.com>
2023-05-10 23:00:54 +0900

    chore(): delete unused methods

--- src/main/java/next/controller/Controller.java
+++ src/main/java/next/controller/Controller.java
@@ -1,29 +1,11 @@
 package next.controller;
 
 import next.view.ModelAndView;
-import next.view.View;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Locale;
 
 
 public interface Controller {
     ModelAndView execute(HttpServletRequest request, HttpServletResponse response) throws Exception;
-
-    default boolean isGet(HttpServletRequest request) {
-        return Method.valueOf(request.getMethod().toUpperCase(Locale.ROOT)).equals(Method.GET);
-    }
-
-    default boolean isPost(HttpServletRequest request) {
-        return Method.valueOf(request.getMethod().toUpperCase(Locale.ROOT)).equals(Method.POST);
-    }
-
-    default boolean isDelete(HttpServletRequest request) {
-        return Method.valueOf(request.getMethod().toUpperCase(Locale.ROOT)).equals(Method.DELETE);
-    }
-
-    default boolean isPut(HttpServletRequest request) {
-        return Method.valueOf(request.getMethod().toUpperCase(Locale.ROOT)).equals(Method.PUT);
-    }
 }
